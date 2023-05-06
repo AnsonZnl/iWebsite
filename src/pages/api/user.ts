216599726233272551c6e3ed9ca1fc834414c207
zhangningle <1160030724@qq.com>
2023-05-06 17:10:51 +0800

    feat: test KV

---
+++ src/pages/api/user.ts
@@ -0,0 +1,10 @@
+import kv from '@vercel/kv';
+import { NextApiRequest, NextApiResponse } from 'next';
+ 
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  const user = await kv.hgetall('user:me');
+  return response.status(200).json(user);
+}
