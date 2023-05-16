6ab8c13b9b37aa3a2f9e0b8293f3a65c727d44be
zhangningle <1160030724@qq.com>
2023-05-16 16:51:30 +0800

    feat: about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,10 +5,11 @@ export default function About() {
   return (
     <main className="">
       <Header />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <h1>关于我们</h1>
-        </div>
+      <div className="max-w-7xl mx-auto px-4 py-2 h-96">
+        <h2 className="">iWebsite是什么？</h2>
+        <p>
+          他是一个基于Next.js、Vercel搭建的网站模版，你可以使用它快速、免费的搭建属于自己的网站，利用Next.js不仅对SEO友好，可以基于Vercel的CI/CD自动化部署。
+        </p>
       </div>
       <Footer />
     </main>
